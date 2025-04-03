bb25231a0d64b4dcf263381cf85dec604307b4a2
NoMeLlamoDante <eo.zm24@outlook.com>
2025-04-03 03:05:45 -0600

    - agregar preguntas y respuestas - agregadas funciones para acortar texto

--- app.js
+++ app.js
@@ -1,62 +1,118 @@
 document.body.onload = initial;
 
 function initial() {
-    const dblocalStorage = window.localStorage;
-    const default_theme = dblocalStorage.getItem("theme");
+    const default_theme = get_theme();
     if (default_theme === null){
-        //Get System theme
+        //Get System default theme
         if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
             // Dark Mode
-            dblocalStorage.setItem("theme", "dark");
-            root.dataset.theme = "dark";
+            set_theme("dark");
         } else {
-            root.dataset.theme = "light";
-            dblocalStorage.setItem("theme", "light");
+            set_theme("light");
         }
     } else {
-        const root = document.documentElement;
-        root.dataset.theme = default_theme;
+        set_theme(default_theme);
     }
 }
 
 function toggleTheme() {
-    const root = document.documentElement;
-    if (root.dataset.theme === "light") {
-        root.dataset.theme = "dark";
-        localStorage.setItem("theme", "dark");
+    const theme = get_theme();
+    if (theme === "light") {
+        set_theme("dark");
     } else {
-        root.dataset.theme = "light";
-        localStorage.setItem("theme", "light");
+        set_theme("light");
     }
 }
 
 function toggleSidebar() {
-    const data = document.querySelector(".sidebar").classList.toggle("closed")
+    const data = QS(".sidebar").classList.toggle("closed")
     if (data)
-        document.getElementById("toggle-chat").style.display = "block";
+        get_by_ID_style("toggle-chat").display = "block";
     else
-        document.getElementById("toggle-chat").style.display = "none";
+        get_by_ID_style("toggle-chat").display = "none";
 }
 
-const entrada = document.querySelector("input");
-entrada.addEventListener("keyup", function (event) {
-    const input_value = document.getElementById("question").value;
+QS("input").addEventListener("keyup", function (event) {
+    const input = get_by_ID("question");
+    const length_value = input.value.length;
     // Enter Key
     if (event.key === 'Enter') {
-        alert("enter pressed");
-        
-    }else {
-        const length_value = input_value.length;
-        if (length_value == 0){
-            document.querySelector(".buttons").style.display = "flex";
-            document.querySelector(".answers").style.display = "none";
-        }else{
-            document.getElementById("1").innerText = input_value;
-            document.getElementById("2").innerText = input_value;
-            document.getElementById("3").innerText = input_value;
-            document.getElementById("4").innerText = input_value;
-            document.querySelector(".buttons").style.display = "none";
-            document.querySelector(".answers").style.display = "flex";
+        if (length_value > 0) {
+            // Desaparecer contenedores
+            QS_style(".data-container").display = "none";
+            QS_style(".buttons").display = "none";
+            QS_style(".asks-hints").display = "none";
+            QS_style("footer").display = "none";
+            
+            // Cambiar al nuevo estilo
+            const container = get_by_ID("chat");
+            container.style.display = "flex";
+            container.style.flexDirection = "column";
+            container.style.justifyContent = "space-between";
+
+            // Container answer
+            let answers_container = QS(".answers");
+            answers_container.display = "flex";
+
+            // Print question
+            const question = document.createElement("p");
+            question.className = "p_question";
+            question.innerText = input.value;
+            answers_container.append(question);
+            
+            // Print answer
+            const answer = document.createElement("p");
+            answer.className = "p_answer";
+            answer.innerText = "Lorem ipsum dolor sit amet \
+            consectetur adipisicing elit. \Excepturi ea porro unde, id debitis tempore \
+            est qui quo quasi, maxime natus! Modi libero \
+            architecto fugiat optio aspernatur provident deserunt error.";
+            answers_container.append(answer);
+
+            input.value = "";
         }
+    } else {
+        // Alter buttons and ask-hints
+        const answers_is_visible = get_css_attrib(".buttons").display;
+        if (answers_is_visible === "flex") {
+            if (length_value == 0) {
+                QS(".buttons").style.display = "flex";
+                QS(".asks-hints").style.display = "none";
+            } else {
+                get_by_ID("1").innerText = input.value;
+                get_by_ID("2").innerText = input.value;
+                get_by_ID("3").innerText = input.value;
+                get_by_ID("4").innerText = input.value;
+                QS(".buttons").style.display = "none";
+                QS(".asks-hints").style.display = "flex";
+            }
+        }
+        
     }
-})
+});
+
+function QS(class_name) {
+    return document.querySelector(class_name);
+}
+function QS_style(class_name) {
+    return QS(class_name).style;
+}
+function get_by_ID(class_name) {
+    return document.getElementById(class_name);
+}
+function get_by_ID_style(class_name) {
+    return get_by_ID(class_name).style;
+}
+function get_css_attrib(class_name) {
+    return window.getComputedStyle(document.querySelector(class_name));
+}
+function get_theme() {
+    const dblocalStorage = window.localStorage;
+    return dblocalStorage.getItem("theme");
+}
+function set_theme(theme) {
+    const dblocalStorage = window.localStorage;
+    const root = document.documentElement;
+    dblocalStorage.setItem("theme", theme);
+    root.dataset.theme = theme;
+}
