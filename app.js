1d062c2ccb943dc83a07f892992620e38dd5e445
NoMeLlamoDante <eo.zm24@outlook.com>
2025-04-02 22:59:21 -0600

    - key events, enter key and text enter

--- app.js
+++ app.js
@@ -9,9 +9,32 @@ function toggleTheme() {
 }
 
 function toggleSidebar() {
-    let data = document.querySelector(".sidebar").classList.toggle("closed")
+    const data = document.querySelector(".sidebar").classList.toggle("closed")
     if (data)
         document.getElementById("toggle-chat").style.display = "block";
     else
         document.getElementById("toggle-chat").style.display = "none";
 }
+
+const entrada = document.querySelector("input");
+entrada.addEventListener("keyup", function (event) {
+    const input_value = document.getElementById("question").value;
+    // Enter Key
+    if (event.key === 'Enter') {
+        alert("enter pressed");
+        
+    }else {
+        const length_value = input_value.length;
+        if (length_value == 0){
+            document.querySelector(".buttons").style.display = "flex";
+            document.querySelector(".answers").style.display = "none";
+        }else{
+            document.getElementById("1").innerText = input_value;
+            document.getElementById("2").innerText = input_value;
+            document.getElementById("3").innerText = input_value;
+            document.getElementById("4").innerText = input_value;
+            document.querySelector(".buttons").style.display = "none";
+            document.querySelector(".answers").style.display = "flex";
+        }
+    }
+})
